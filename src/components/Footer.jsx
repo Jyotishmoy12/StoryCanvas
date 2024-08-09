d7a46549cecd46dcf6f6a3ec0dfaf73071899b61
Jyotishmoy12 <93263133+Jyotishmoy12@users.noreply.github.com>
2024-08-09 23:15:10 +0530

    links added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,14 +6,14 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer-content">
         <div className="footer-social">
-          <a href="#" className="social-icon instagram">
+          <a href="https://www.instagram.com/__jyo__2001?igsh=MTFrMnc5dXNkM296Ng%3D%3D" className="social-icon instagram">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
               <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
               <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
               <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
             </svg>
           </a>
-          <a href="#" className="social-icon linkedin">
+          <a href="https://www.linkedin.com/in/jyotishmoy-deka-6871b9229/" className="social-icon linkedin">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
               <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
               <rect x="2" y="9" width="4" height="12"></rect>
