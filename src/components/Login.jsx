cce55e9139e521c5fba1350b49c672ca789adac5
Jyotishmoy12 <93263133+Jyotishmoy12@users.noreply.github.com>
2024-08-13 10:20:19 +0530

    some bug fixes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,6 +3,7 @@ import { useNavigate, Link } from 'react-router-dom';
 import { signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, sendPasswordResetEmail } from "firebase/auth";
 import { auth } from '../../firebase';
 import '../styles/Auth.css';
+import Navbar from './Navbar';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -44,6 +45,8 @@ const Login = () => {
   };
 
   return (
+    <>
+    <Navbar/>
     <div className="auth-container">
       <div className="glass-card auth-card">
         <h2>Login</h2>
@@ -72,13 +75,16 @@ const Login = () => {
             <path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"/>
             <path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"/>
         </svg>
-        {" Sign up with Google"}
+        {" Log In with Google"}
         </button>
         <p><a href="#" onClick={handleResetPassword}>Forgot password?</a></p>
         <p>Don't have an account? <Link to="/signup">Sign up</Link></p>
       </div>
     </div>
+    </>
   );
+  
 };
+
 
 export default Login;
